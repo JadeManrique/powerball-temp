a9e0ed3cd28664d6e37e5296f59f5139b9019c6b
Jade Manrique <jade.a.manrique@gmail.com>
2018-04-25 12:49:09 -0700

    .css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,12 @@
 (document).ready( function() {
 
-  $(".button").click(function() {
-    
+  $(".submit").click(function() {
+
   });
 
 });
+
+function limiter(input) {
+   if (input.value < 0) input.value = 0;
+   if (input.value > 10000000) input.value = 10000000;
+}
