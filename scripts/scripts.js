09a9aa770763282baa290380021a5d172efdb990
Jade Manrique <jade.a.manrique@gmail.com>
2018-04-26 20:19:41 -0700

    .css list

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,17 @@
-(document).ready( function() {
+$(document).ready( function() {
 
   $(".submit").click(function() {
-
+    var entry = {
+      winnings: $("#currentNum").val(),
+      name: $("#name").val(),
+      cash: $("#money").val()
+    }
+    console.log(entry);
   });
+
+  function playGame(data) {
+   $.post('https://lottery-vs-reality-eagarcia.c9users.io/playGame', data);
+ }
 
 });
 
